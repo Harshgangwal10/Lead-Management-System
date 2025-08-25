dc24f78756196aa04783fc5525c48a2a80209049
Harsh Gangwal <harshgangwal24@gmail.com>
2025-08-25 13:19:02 +0530

    made changes in util.js

--- config/utils.js
+++ config/utils.js
@@ -7,7 +7,7 @@ export const generateToken =(userId,res)=>{
    res.cookie("jwt" , token,{
    maxAge:7 * 24 * 60 * 60 * 1000,
      httpOnly: true,
-     sameSite:"strict",
+     sameSite:process.env.NODE_ENV === "production" ? "none" : "lax",
      secure:process.env.NODE_ENV !=="development",
    });
 
